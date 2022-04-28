63bcc505b71ce9b0fa7b6319c0bc51d56bf67283
Rafael Graunke <rafael.graunke@yahoo.com>
2022-04-28 16:05:04 -0300

    Adicionada rota para buscar entidade por id

--- src/routes/entidade.js
+++ src/routes/entidade.js
@@ -4,5 +4,6 @@ import entidadeControllers from '../controllers/EntidadeController';
 const router = new Router();
 
 router.get('/', entidadeControllers.index);
+router.get('/:id', entidadeControllers.show);
 
 export default router;
