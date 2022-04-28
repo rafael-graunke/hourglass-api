633ecb238f8b7b93cfc42ec11e295edcb4792412
Rafael Graunke <rafael.graunke@yahoo.com>
2022-04-28 15:49:16 -0300

    Adiciona busca de entidade por id

--- src/controllers/EntidadeController.js
+++ src/controllers/EntidadeController.js
@@ -15,6 +15,21 @@ class EntidadeController {
       console.error(e);
     }
   }
+
+  async show(req, res) {
+    try {
+      const entidades = await glpiDatabase.query(
+        'SELECT id, name FROM `glpi_entities` WHERE id = ?',
+        {
+          replacements: [req.params.id],
+          type: QueryTypes.SELECT,
+        }
+      );
+      return res.json(entidades[0]);
+    } catch (e) {
+      console.error(e);
+    }
+  }
 }
 
 export default new EntidadeController();
