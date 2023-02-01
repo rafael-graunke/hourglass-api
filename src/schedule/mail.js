3e9123510dfe4f21e09d8b4f896d4d5ae43c09d0
Rafael Graunke <rafael.graunke@outlook.com>
2023-02-01 13:07:41 -0300

    fix: conversao de segundos para tempo

--- src/schedule/mail.js
+++ src/schedule/mail.js
@@ -4,14 +4,14 @@ import ejs from 'ejs';
 import nodemailer from 'nodemailer';
 import config from '../config/config';
 
-function toHHMMSS(segundos) {
-  const hours = Math.floor(segundos / 3600);
-  let minutes = Math.floor((segundos - hours * 3600) / 60);
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
+function toHHMMSS (secs) {
+  let sec_num = parseInt(secs, 10);
+  let hours   = Math.floor(sec_num / 3600);
+  let minutes = Math.floor((sec_num - (hours * 3600)) / 60);
 
-  return `${hours}:${minutes}`;
+  if (hours   < 10) {hours   = "0"+hours;}
+  if (minutes < 10) {minutes = "0"+minutes;}
+  return hours+':'+minutes+'h';
 }
 
 async function enviaTodosEmails(entidades) {
